1b32603bc45b5e41d2b04788876cbeabc1353580
Danny Caesar <yadenis@list.ru>
2018-09-12 17:28:33 +0300

    Register page enhancements

--- src/components/login/RegistrationComponent/RegistrationComponent.js
+++ src/components/login/RegistrationComponent/RegistrationComponent.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+
+import './RegistrationComponent.scss';
 
 class RegistrationComponent extends Component {
 	constructor(){
@@ -10,7 +14,8 @@ class RegistrationComponent extends Component {
 			username: '',
 			password: '',
 			confirm_password: '',
-			message: ''
+			message: '',
+			redirect: false
 		}
 	}
 
@@ -24,11 +29,20 @@ class RegistrationComponent extends Component {
 	setPassword = (e) => { this.setState({ password: e.target.value }); }
 	setConfirmPassword = (e) => { this.setState({ confirm_password: e.target.value }) }    
 
-	frontValidation = (e) => {
-		e.preventDefault();
+	closeMessage = () => {
+		this.setState({ message: '' });
+	}
+
+	frontValidation = () => {
 		let error = false;
-		if (this.state.password !== this.state.confirm_password) {
+
+		if (this.state.name.length === 0) { this.setState({ message: "Name field is empty" }); error = true; }
+		else if (this.state.surname.length === 0) { this.setState({ message: "Surname field is empty" }); error = true; }
+		else if (this.state.username.length === 0) { this.setState({ message: "Username field is empty" }); error = true; }
+		else if (this.state.password.length === 0) { this.setState({ message: "Password field is empty" }); error = true; }
+		else if (this.state.password !== this.state.confirm_password) {
 			this.setState({ message: "Passwords do not match!" });
+			error = true;
 		}
 
 		if (!error) {
@@ -43,24 +57,65 @@ class RegistrationComponent extends Component {
 			axios.post('/auth/registration', {
 				data: data
 			});
+
+			this.setState({ redirect: true });
 		}
 	}
 
 	render(){
 		return (
-			<div>
-				RegistrationComponent
-				{this.state.message.length !== 0 ? 
-					<div>{this.state.message}</div>
-				: null }
-				<form method="POST" action="/registration">
-					<input type="text" placeholder="Name" defaultValue={this.state.name} onChange={this.setName} />
-					<input type="text" placeholder="Surname" defaultValue={this.state.surname} onChange={this.setSurname} />
-					<input type="text" placeholder="Username" required defaultValue={this.state.username} onChange={this.setUsername} />
-					<input type="text" placeholder="Password" required defaultValue={this.state.password} onChange={this.setPassword} />
-					<input type="text" placeholder="Confirm password" required defaultValue={this.state.confirm_password} onChange={this.setConfirmPassword} />
-					<button onClick={this.frontValidation}>Register</button>
-				</form>
+			<div className="registration-component">
+				
+				<div className="registration-component__logo_wrapper">
+					<div className="registration-component__logo"></div>
+					<div className="registration-component__logo-char"></div>
+				</div>
+
+				<div className="registration-component__form col-xs-10 col-sm-6">
+
+					<div className="registration-component__form-block">
+						<span className="registration-component__icon"><i className="fas fa-user"></i></span>
+						<input className="registration-component__input" type="text" placeholder="Name" defaultValue={this.state.name} onChange={this.setName} />
+					</div>
+
+					<div className="registration-component__form-block">
+						<span className="registration-component__icon"><i className="fas fa-user"></i></span>
+						<input className="registration-component__input" type="text" placeholder="Surname" defaultValue={this.state.surname} onChange={this.setSurname} />
+					</div>
+
+					<div className="registration-component__form-block">
+						<span className="registration-component__icon"><i className="fas fa-user"></i></span>
+						<input className="registration-component__input" type="text" placeholder="Username" required defaultValue={this.state.username} onChange={this.setUsername} />
+					</div>
+
+					<div className="registration-component__form-block">
+					<span className="registration-component__icon"><i className="fas fa-lock"></i></span>
+						<input className="registration-component__input" type="password" placeholder="Password" required defaultValue={this.state.password} onChange={this.setPassword} />
+					</div>
+
+					<div className="registration-component__form-block">
+						<span className="registration-component__icon"><i className="fas fa-lock"></i></span>
+						<input className="registration-component__input" type="password" placeholder="Confirm password" required defaultValue={this.state.confirm_password} onChange={this.setConfirmPassword} />
+					</div>
+
+					<div className="registration-component__button" onClick={this.frontValidation}><i className="fas fa-angle-right"></i></div>
+					<div className="registration-component__form-footer">
+						<span>Already have an account? <Link to="/login"><span>Sign in</span></Link></span>
+					</div>
+
+					{this.state.message.length !== 0 ? 
+					<div className="registration-component__message-container">
+						<div className="registration-component__message">
+							{this.state.message}
+						</div>
+						<div className="registration-component__message_close"><i className="fas fa-times" onClick={this.closeMessage}></i></div>
+					</div>
+					: null }
+
+					{this.state.redirect ?
+						<Redirect to="/login" />
+					: null }
+				</div>
 			</div>
 		)
 	}
