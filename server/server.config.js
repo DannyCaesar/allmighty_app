fd0582a600b22d3db83d9a38fcd60a9845b0d161
Danny Caesar <yadenis@list.ru>
2018-09-27 20:18:36 +0300

    Heroku

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,8 @@
 const CONFIG = {
 	port: process.env.PORT || 3000,
-	mongo_dictionary_db: process.env.MONGO_DICTIONARY_DB || "test_db",
+	//mongo_dictionary_db: process.env.MONGO_DICTIONARY_DB || "test_db",
 	//mongo_port: process.env.MONGO_PORT_DEV || "mongodb://localhost:27017",
+	mongo_dictionary_db: "heroku_0tdhlxcn",
 	mongo_port: "mongodb://dannycaesar:jokerjok77@ds117423.mlab.com:17423/heroku_0tdhlxcn",
 	jwt_expiration_time: 24*60*60*1000,
 	jwt_secret: process.env.JWT_SECRET || "secret"
